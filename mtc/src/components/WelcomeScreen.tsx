254e300848e383b05745abba7048b3f8ac9ec359
JaredBaileyDuke <jared.l.bailey@duke.edu>
2025-02-21 09:55:12 -0500

    Added non-working contact form

--- mtc/src/components/WelcomeScreen.tsx
+++ mtc/src/components/WelcomeScreen.tsx
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
 import './WelcomeScreen.css';
+import ContactUs from './ContactUs';
 
 interface WelcomeScreenProps {
   onHomeClick: () => void;
   onMachineTypeConfigurationClick: () => void;
   onMachineHealthClick: () => void;
-  onContactUsClick: () => void;
 }
 
-export default function WelcomeScreen( { onMachineTypeConfigurationClick, onMachineHealthClick, onContactUsClick }: WelcomeScreenProps ) {
+export default function WelcomeScreen( { onMachineTypeConfigurationClick, onMachineHealthClick }: WelcomeScreenProps ) {
+  const [showContactUs, setShowContactUs] = useState(false);
+
+  const handleContactUsClick = () => {
+    setShowContactUs(true);
+  };
+
+  const handleBackClick = () => {
+    setShowContactUs(false);
+  };
+
+  if (showContactUs) {
+    return <ContactUs onBackClick={handleBackClick} />;
+  }
+
   return (
       <div className="welcome-screen">
         <h1>Your Personal Machine Assistant</h1>
@@ -20,7 +35,7 @@ export default function WelcomeScreen( { onMachineTypeConfigurationClick, onMach
           <button>Optimize Machine Data</button>
         </div>
         <div className="button-container centered">
-          <button onClick={onContactUsClick}>Contact Us</button>
+          <button onClick={handleContactUsClick}>Contact Us</button>
         </div>
       </div>
   );
